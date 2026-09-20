2a0029ede40836c773b911eddfd4950a956fab33
GamaGamer <lav.mlinar@gmail.com>
2026-09-20 11:38:07 +0200

    Implement extra jumps

---
+++ scenes/pickups.gd
@@ -0,0 +1,41 @@
+extends TileMapLayer
+
+signal reset_pickups
+
+const RESET_TIME := 2.5
+const PICKUP_COORDS := [Vector2i(0, 1), Vector2i(1, 1)]
+
+var times := []
+var spent_pickups := []
+# [coords, atlas_coords.x]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	for i in range(times.size()):
+		times[i] += delta
+	while times.size() > 0 and times[0] >= RESET_TIME:
+		reset_pickup(spent_pickups[0][0], spent_pickups[0][1])
+		times.pop_front()
+		spent_pickups.pop_front()
+
+func reset_pickup(coords: Vector2i, atlas_x: int) -> void:
+	set_cell(coords, 2, Vector2i(atlas_x, 1))
+
+func queue_pickup(pos: Vector2) -> void:
+	var map_pos := local_to_map(pos)
+	var atlas_coords := get_cell_atlas_coords(map_pos)
+	set_cell(map_pos, 2, Vector2i(atlas_coords.x, 0))
+	times.push_back(0.0)
+	spent_pickups.push_back([map_pos, atlas_coords.x])
+
+func is_pickup(pos: Vector2) -> bool:
+	return get_cell_source_id(local_to_map(pos)) == 2 and get_cell_atlas_coords(local_to_map(pos)) in PICKUP_COORDS
+
+func _on_reset_pickups() -> void:
+	for pickup in spent_pickups:
+		set_cell(pickup[0], 2, Vector2i(pickup[1], 1))
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,13 +4,16 @@ signal died
 
 @onready var anim_sprite := $AnimatedSprite2D
 @onready var hurtbox := $Hurtbox
+@onready var healbox := $Healbox
 @onready var jump_counter := $JumpCounter
+@onready var tilemap = get_parent().get_node("TileMapLayer")
 @onready var spawn_pos := global_position
+@onready var healbox_ssh : Vector2 = get_node("Healbox").get_node("CollisionShape2D").shape.size / 2.0
 
 const SPEED := 250.0
 const GRAVITY := 1.25
-const JUMP_VELOCITY := -300.0
-const JUMP_EXT_VELOCITY := -30.0
+const JUMP_VELOCITY := -250.0
+const JUMP_EXT_VELOCITY := -25.0
 const MAX_JUMP_TIME := 0.25
 const COYOTE_TIME := 0.1
 
@@ -18,7 +21,9 @@ var jump_timer := 0.0
 var coyote_timer := COYOTE_TIME
 var jumped := false
 var input := false
+var locked := false
 var extra_jumps := 0
+var picking_up := false
 
 func _process(delta: float) -> void:
 	if extra_jumps:
@@ -28,7 +33,7 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	
-	if not is_on_floor():
+	if not is_on_floor() and not locked:
 		velocity += get_gravity() * delta * GRAVITY
 		coyote_timer += delta
 	else:
@@ -58,37 +63,64 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	if direction == -1:
-		anim_sprite.flip_h = true
-	elif direction == 1:
-		anim_sprite.flip_h = false
-	
-	if not is_on_floor():
-		if velocity.y < 0:
-			anim_sprite.play("jump_up")
+	if not locked:
+		if direction == -1:
+			anim_sprite.flip_h = true
+		elif direction == 1:
+			anim_sprite.flip_h = false
+		
+		if not is_on_floor():
+			if velocity.y < 0:
+				anim_sprite.play("jump_up")
+			else:
+				anim_sprite.play("jump_down")
+		elif direction:
+			anim_sprite.play("run")
 		else:
-			anim_sprite.play("jump_down")
-	elif direction:
-		anim_sprite.play("run")
-	else:
-		anim_sprite.play("idle")
+			anim_sprite.play("idle")
 	
 	move_and_slide()
 	
 	if global_position.y >= 650:
 		died.emit()
 	
-	if not input and is_on_floor():
+	if not input and is_on_floor() and not locked:
 		input = true
 
 
 func _on_died() -> void:
-	global_position = spawn_pos
-	velocity = Vector2(0, 0)
-	jump_timer = 0.0
-	coyote_timer = COYOTE_TIME
-	jumped = false
-	input = false
+	if not locked:
+		velocity = Vector2(0, 0)
+		input = false
+		locked = true
+		anim_sprite.play("death")
+		await anim_sprite.animation_looped
+		global_position = spawn_pos
+		velocity = Vector2(0, 0)
+		jump_timer = 0.0
+		coyote_timer = COYOTE_TIME
+		extra_jumps = 0
+		tilemap.reset_pickups.emit()
+		jumped = false
+		locked = false
 
 func _on_hurtbox_body_entered(_body: Node2D) -> void:
 	died.emit()
+
+
+func _on_healbox_body_entered(_body: Node2D) -> void:
+	if not picking_up:
+		picking_up = true
+		var corners = [
+			healbox.get_node("CollisionShape2D").global_position + Vector2(-healbox_ssh.x, -healbox_ssh.y),
+			healbox.get_node("CollisionShape2D").global_position + Vector2(-healbox_ssh.x, healbox_ssh.y),
+			healbox.get_node("CollisionShape2D").global_position + Vector2(healbox_ssh.x, -healbox_ssh.y),
+			healbox.get_node("CollisionShape2D").global_position + Vector2(healbox_ssh.x, healbox_ssh.y)
+		]
+		for c in corners:
+			var corner = tilemap.to_local(c)
+			if tilemap.is_pickup(corner):
+				extra_jumps = max(extra_jumps, tilemap.get_cell_atlas_coords(tilemap.local_to_map(corner)).x + 1)
+				tilemap.queue_pickup(corner)
+				picking_up = false
+				return
